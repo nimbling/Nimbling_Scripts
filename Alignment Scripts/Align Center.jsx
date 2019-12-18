c25a6679d38c5977796086b5b70a57a5e76017ae
Herman van Boeijen <hvb@xs4all.nl>
2019-12-18 17:37:32 +0100

    Align center added.

---
+++ Alignment Scripts/Align Center.jsx
@@ -0,0 +1,78 @@
+﻿#target Illustrator
+//  script.grandparent = carlos canto
+//  script.parent = Herman van Boeijen
+//  script.elegant = false;
+
+var idoc = app.activeDocument;
+selec = idoc.selection;
+
+// get document bounds
+var docw = idoc.width;
+var doch = idoc.height;
+
+if (selec.length == 1) {
+    // Align to artboard
+
+    var activeAB = idoc.artboards[idoc.artboards.getActiveArtboardIndex()]; // get active AB
+    docLeft = activeAB.artboardRect[0];
+    docTop = activeAB.artboardRect[1];
+    docRight = activeAB.artboardRect[2];
+    docBottom = activeAB.artboardRect[3];
+
+    // get selection bounds
+    var sel = idoc.selection[0];
+
+    var horziontalCenterPosition = docLeft + ((docRight - sel.width) / 2);
+    var verticalCenterPosition = docTop + ((docBottom + sel.height) / 2);
+
+    sel.position = [horziontalCenterPosition, verticalCenterPosition];
+
+} else if (selec.length > 1) {
+    var alignsH = [];
+    var alignsV =[];
+    var alignsT =[];
+    var alignsB =[];
+    
+
+    for (var i = 0; i < selec.length; i++) {
+        // get selection bounds
+        var sel = idoc.selection[i];
+        var selHC = sel.left + sel.width / 2;
+        var selVC = sel.Top + sel.height / 2;
+
+        alignsV.push(selVC);
+        alignsH.push(selHC);
+    }
+
+    for (var i = 0; i < selec.length; i++) {
+        alignsT.push(selec[i].top);
+    }
+    aligntop = Math.max.apply(null, alignsT);
+
+    for (var i = 0; i < selec.length; i++) {
+        // get selection bounds
+        var sel = idoc.selection[i];
+        bottom = selec[i].top - sel.height;
+        alignsB.push(bottom);
+    }
+    alignbot = Math.min.apply(null, alignsB);
+
+    var align = (aligntop + alignbot)/2;
+
+    alignLeft = Math.min.apply(null, alignsH);
+    alignRight = Math.max.apply(null, alignsH);
+
+    alignH = (alignLeft + alignRight)/2;
+
+    for (var i = 0; i < selec.length; i++) {
+        var sel = idoc.selection[i];
+
+        sel.position = [alignH - (sel.width / 2), align + (sel.height / 2)];
+    }
+
+
+
+
+} else {
+    alert("no object(s) selected")
+}
